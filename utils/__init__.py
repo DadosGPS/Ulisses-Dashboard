1ac30e356e8907affdd36debc78218e70e490396
DadosGPS <ulissescv99@gmail.com>
2026-05-02 13:08:14 +0100

    Add files via upload

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,3 @@
-import sys, os
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__))))
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from utils.dados import *
-from utils.calculos import *
-from utils.ui import *
+from utils.dados import carregar_dados, carregar_dados_safe, carregar_exercicios, get_mets_gps, normalizar_coluna, COL_ALIASES
+from utils.calculos import calcular_acwr, calcular_acwr_global, zscore_serie, cor_acwr, calcular_monotonia_strain
+from utils.ui import lm_header, premium_layout, botao_download_html, gerar_pdf_html, metric_card, sem_dados_suficientes
