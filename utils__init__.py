8423be2771029ccbbd4f4f09cdc83f1603f1bfea
DadosGPS <ulissescv99@gmail.com>
2026-05-02 11:40:26 +0100

    Delete utils__init__.py

--- utils__init__.py
+++
@@ -1,3 +0,0 @@
-from .dados import carregar_dados, carregar_dados_safe, carregar_exercicios, get_mets_gps, normalizar_coluna, COL_ALIASES
-from .calculos import calcular_acwr, calcular_acwr_global, zscore_serie, cor_acwr, calcular_monotonia_strain
-from .ui import lm_header, premium_layout, botao_download_html, gerar_pdf_html, metric_card, sem_dados_suficientes
