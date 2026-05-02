0e109223b88a31ab3b66fe8ac27ab881ec7bc021
DadosGPS <ulissescv99@gmail.com>
2026-05-02 12:23:08 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,9 @@
 import sys, os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+_utils_dir = os.path.dirname(os.path.abspath(__file__))
+_root_dir = os.path.dirname(_utils_dir)
+if _utils_dir not in sys.path: sys.path.insert(0, _utils_dir)
+if _root_dir not in sys.path: sys.path.insert(0, _root_dir)
 
-from dados import carregar_dados, carregar_dados_safe, carregar_exercicios, get_mets_gps, normalizar_coluna, COL_ALIASES
-from calculos import calcular_acwr, calcular_acwr_global, zscore_serie, cor_acwr, calcular_monotonia_strain
-from ui import lm_header, premium_layout, botao_download_html, gerar_pdf_html, metric_card, sem_dados_suficientes
+from utils.dados import carregar_dados, carregar_dados_safe, carregar_exercicios, get_mets_gps, normalizar_coluna, COL_ALIASES
+from utils.calculos import calcular_acwr, calcular_acwr_global, zscore_serie, cor_acwr, calcular_monotonia_strain
+from utils.ui import lm_header, premium_layout, botao_download_html, gerar_pdf_html, metric_card, sem_dados_suficientes
